dd2c39657317a4ec6eef873a0b045dcfc14af446
RhodesZ <445368386@qq.com>
2014-09-16 19:10:32 +0800

    客户端1.23
    1.数据库SQL语言重写。使用更规范的SQL语句替换原来的语句
    2.更新userinfo数据库，添加permission列，为不同的用户设置不同的权限。具体权限在1.3中发布
    3.界面微调。

---
+++ 客户端1.23/客户端/advice.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace 客户端
+{
+    partial class Advice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cancelbutton = new System.Windows.Forms.Button();
+            this.submitbutton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // subject
+            // 
+            this.subject.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.subject.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.subject.Location = new System.Drawing.Point(72, 99);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(478, 20);
+            this.subject.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(58)))), ((int)(((byte)(78)))));
+            this.label2.Location = new System.Drawing.Point(20, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "标题：";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::客户端.Properties.Resources.textbox5;
+            this.pictureBox6.Location = new System.Drawing.Point(13, 89);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(550, 40);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 38;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::客户端.Properties.Resources.装饰8;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(552, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::客户端.Properties.Resources.list;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 147);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(550, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 53;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cancelbutton
+            // 
+            this.cancelbutton.BackgroundImage = global::客户端.Properties.Resources.button2;
+            this.cancelbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cancelbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cancelbutton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelbutton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cancelbutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.cancelbutton.Location = new System.Drawing.Point(445, 282);
+            this.cancelbutton.Name = "cancelbutton";
+            this.cancelbutton.Size = new System.Drawing.Size(119, 40);
+            this.cancelbutton.TabIndex = 4;
+            this.cancelbutton.Text = "Cancel";
+            this.cancelbutton.UseVisualStyleBackColor = true;
+            this.cancelbutton.Click += new System.EventHandler(this.cancelbutton_Click);
+            this.cancelbutton.MouseEnter += new System.EventHandler(this.cancelbutton_MouseEnter);
+            this.cancelbutton.MouseLeave += new System.EventHandler(this.cancelbutton_MouseLeave);
+            // 
+            // submitbutton
+            // 
+            this.submitbutton.BackgroundImage = global::客户端.Properties.Resources.button;
+            this.submitbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.submitbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.submitbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.submitbutton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.submitbutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.submitbutton.Location = new System.Drawing.Point(324, 282);
+            this.submitbutton.Name = "submitbutton";
+            this.submitbutton.Size = new System.Drawing.Size(118, 40);
+            this.submitbutton.TabIndex = 3;
+            this.submitbutton.Text = "Submit";
+            this.submitbutton.UseVisualStyleBackColor = true;
+            this.submitbutton.Click += new System.EventHandler(this.submitbutton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(25, 159);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(525, 98);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Advice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(576, 343);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.cancelbutton);
+            this.Controls.Add(this.submitbutton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Advice";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "advice";
+            this.Load += new System.EventHandler(this.Advice_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Advice_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Advice_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox subject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button cancelbutton;
+        private System.Windows.Forms.Button submitbutton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+
+    }
+}
